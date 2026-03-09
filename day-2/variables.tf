b54f7087f2ab46708153f5963ea6835fcec14a53
siddarth <siddarth.182772@gmail.com>
2026-03-09 10:43:47 +0530

    day-2

---
+++ day-2/variables.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+      description = "ami id for the instance"
+      default = ""
+      type = string
+}
+variable "instance_type" {
+      description = "instance type for the instance"
+      default = ""
+      type = string
+}
