14f45cc6a1962a430f2e6e0db762104f29c7b342
siddarth <siddarth.182772@gmail.com>
2026-03-13 12:28:24 +0530

    day - 6 one

--- day-6 statelock-1/main.tf
+++ day-6 statelock-1/main.tf
@@ -1,10 +1,56 @@
 resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = "tf-vpc"
+    Name = "hello-vpc"
   }
 }
+
 resource "aws_subnet" "name" {
   cidr_block = "10.0.0.0/24"
+  vpc_id     = aws_vpc.name.id
+
+  tags = {
+    Name = "hello-subnet"
+  }
+}
+
+resource "aws_subnet" "hero" {
+  cidr_block = "10.0.2.0/26"
+  vpc_id     = aws_vpc.name.id
+
+  tags = {
+    Name = "hello"
+  }
+}
+
+resource "aws_internet_gateway" "ig" {
   vpc_id = aws_vpc.name.id
+
+  tags = {
+    Name = "hello-ig"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.name.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    Name = "hello-rt"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.name.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.hero.id
+  route_table_id = aws_route_table.rt.id
 }
